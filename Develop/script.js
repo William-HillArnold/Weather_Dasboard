65a9813dbad0121711799d42a2a1b48a49420ac8
williamhillarnold@yahoo.com <97689293+will9105@users.noreply.github.com>
2022-07-07 23:25:42 -0400

    file fix for develop folder

--- Develop/script.js
+++ Develop/script.js
@@ -9,17 +9,37 @@
  var uvIndex = document.querySelector('uvIndex');
  const time = document.getElementById('time');
 
-  function getCity(){
-      const newCity = document.getElementById('cityInput');
-      const cityName = document.getElementById('cityName');
-      cityName.innerHTML = newName.value
-       console.log(newCity);
-  }
+//   function getCity(){
+//       const newCity = document.getElementById('cityInput');
+//       const cityName = document.getElementById('cityName');
+//       cityName.innerHTML = newName.value
+//        console.log(newCity);
+//   }
 
 button.addEventListener('click', function() {
-fetch ('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&APPID=45bf08bcabf42c4645ac65d3c9686f82')
+fetch ('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&APPID=45bf08bcabf42c4645ac65d3c9686f82&units=imperial')
  .then(response => response.json())
- .then (data => console.log(data)) 
+ .then (data => {
+     console.log(data);
+    var cityValue = data['name'];
+    var tempValue = data['main']['temp'];
+    var humidityValue = data['main']['humidity'];
+    var maxTempValue = data ['main']['temp_max'];
+    //var minTempValue = data['main']['temp_min'];
+    var descriptionValue = data['weather']['description'];
+     //var windValue = data['wind'];
+    
+    
+     city.innerHTML = cityValue;
+     temp.innerHTML = tempValue;
+     humidity.innerHTML = humidityValue;
+     description.innerHTML = descriptionValue
+     //wind.innerHTML = windValue;
+     //tempMax.innerHTML = maxTempValue;
+     //tempMin.innerHTML = minTempValue;
+    // fetch()
+    )
+ }) 
 
- .catch(err => alert('Enter a city!'))
- })
+ 
+ })       
